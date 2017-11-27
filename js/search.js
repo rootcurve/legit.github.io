d12c4f39fc2aa1de61c708048f439b45b6709215
Legit <legit@sent.com>
2017-11-27 00:13:41 -0500

    Update search.js

--- js/search.js
+++ js/search.js
@@ -8,8 +8,8 @@
             for (var i = 0; i < results.length; i++) {  // Iterate over the results
                 var item = store[results[i].ref];
                 appendString += '<div class="post-preview"><a href="' + item.url + '"><h2 class="post-title">' + item.title + '</h2>';
-                appendString += item.excerpt + '</a>';
-                appendString += '<p class="post-meta">Posted by ' +  item.author + 'on ' + item.date;
+                //appendString += item.excerpt + '</a>';
+                //appendString += '<p class="post-meta">Posted by ' +  item.author + 'on ' + item.date;
                 appendString += '<hr>';
             }
 
